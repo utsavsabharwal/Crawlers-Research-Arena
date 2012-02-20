b51c8a5522da00ec387e8a7181ef3a0a155273a1
Utsav Sabharwal <utsavsabharwal@live.com>
2012-02-20 16:05:40 +0530

    New Template

--- curl/template/output/autorun.sh
+++
@@ -1,8 +0,0 @@
-python create_db_input.py 
-mysql -uroot -h10.241.31.96 spider -e "select url, product_id from url_queue where is_disabled=0 and last_fetch=0" >> product_map
-mysql -uroot -h10.241.31.96 spider -e "select distinct rdomain, url from url_queue where is_disabled=0 and last_fetch=0" >> "urls"
-python disable.py
-mysql -h10.241.31.96 spider < d &
-python enable.py &
-python insert.py
-mysql -h10.241.31.96 spider < i
